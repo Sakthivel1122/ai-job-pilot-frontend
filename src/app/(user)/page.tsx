086498f8ae16c34333a6c801f2e645611a81f7bf
Sakthivel1122 <asakthivel1122@gmail.com>
2025-11-20 16:25:39 +0530

    theme provider code added

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,16 +1,16 @@
 import styles from "./page.module.scss";
 import LandingPage from "@/pages/landingPage/landingPage";
-// import { getServerSession } from "next-auth";
-// import { authOptions } from "@/lib/auth";
-// import { redirect } from "next/navigation";
-// import { ROUTES } from "@/constants/app-constants";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { ROUTES } from "@/constants/app-constants";
 
 const Home = async () => {
-  // const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-  // if (session?.isLoggedIn) {
-  //   redirect(ROUTES.DASHBOARD);
-  // }
+  if (session?.isLoggedIn) {
+    redirect(ROUTES.DASHBOARD);
+  }
 
   return (
     <>
